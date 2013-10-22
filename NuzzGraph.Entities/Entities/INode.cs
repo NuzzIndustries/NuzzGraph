1ace577374d191c3e8a656940bfc39d056a29870
nuzz604 <nuzz604@gmail.com>
2013-10-22 01:16:40 -0700

    Initial commit

---
+++ NuzzGraph.Entities/Entities/INode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BrightstarDB.EntityFramework;
+
+namespace NuzzGraph.Entities
+{
+    [Entity]
+    public interface INode
+    {
+        /// <summary>
+        /// Get the persistent identifier for this entity
+        /// </summary>
+        string Id { get; }
+
+        // TODO: Add other property references here
+    }
+}
